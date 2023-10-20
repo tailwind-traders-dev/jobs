21efdc45053c86d0b991e7a6ab9bddc1afb5b8dd
Aaron Wislang <aaron.wislang@microsoft.com>
2023-10-19 22:14:40 -0400

    Add Storage, Key Vault
    Signed-off-by: Aaron Wislang <aaron.wislang@microsoft.com>

--- go.mod
+++ go.mod
@@ -8,7 +8,10 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.7.1 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.3.1 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.3.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets v0.12.0 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/keyvault/internal v0.7.1 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus v1.4.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.2.0 // indirect
 	github.com/Azure/go-amqp v1.0.2 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.1.1 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
--- storage.go
+++ storage.go
@@ -10,11 +10,87 @@ import (
 	"os"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/keyvault/azsecrets"
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/magefile/mage/mg"
 )
 
 type Azure mg.Namespace
+
+func (Azure) Storage() error {
+	storageAccountName := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME")
+	if storageAccountName == "" {
+		return errors.New("AZURE_STORAGE_ACCOUNT_NAME environment variable not found")
+	}
+	storageContainerName := os.Getenv("AZURE_STORAGE_CONTAINER_NAME")
+	if storageContainerName == "" {
+		return errors.New("AZURE_STORAGE_CONTAINER_NAME environment variable not found")
+	}
+
+	credential, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return err
+	}
+
+	url1 := fmt.Sprintf("https://%s.blob.core.windows.net/", storageAccountName)
+	client, err := azblob.NewClient(url1, credential, nil)
+	if err != nil {
+		return err
+	}
+
+	pager := client.NewListBlobsFlatPager(storageContainerName,
+		&azblob.ListBlobsFlatOptions{
+			Include: azblob.ListBlobsInclude{
+				Snapshots: true,
+				Versions:  true,
+			},
+		})
+
+	ctx := context.Background()
+	for pager.More() {
+		resp, err := pager.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		for _, blob := range resp.Segment.BlobItems {
+			url2 := fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s", storageAccountName, storageContainerName, *blob.Name)
+			fmt.Println(url2)
+		}
+	}
+	return nil
+}
+
+func (Azure) KeyVault() error {
+	vaultURI := os.Getenv("AZURE_KEY_VAULT_URI")
+	if vaultURI == "" {
+		return errors.New("AZURE_KEY_VAULT_URI environment variable not found")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return err
+	}
+
+	client, err := azsecrets.NewClient(vaultURI, cred, nil)
+	if err != nil {
+		return err
+	}
+
+	ctx := context.Background()
+	pager := client.NewListSecretsPager(nil)
+	for pager.More() {
+		page, err := pager.NextPage(ctx)
+		if err != nil {
+			return err
+		}
+		for _, secret := range page.Value {
+			fmt.Printf("%s\n", *secret.ID)
+		}
+	}
+
+	return nil
+}
 
 func (Azure) ServiceBus() error {
 	queueName := os.Getenv("AZURE_SERVICEBUS_QUEUE_NAME")
@@ -22,8 +98,17 @@ func (Azure) ServiceBus() error {
 		return errors.New("AZURE_SERVICEBUS_QUEUE_NAME environment variable not found")
 	}
 
-	t := Test{}
-	client, err := t.GetServiceBusClientDefault()
+	namespace := os.Getenv("AZURE_SERVICEBUS_HOSTNAME")
+	if namespace == "" {
+		return errors.New("AZURE_SERVICEBUS_HOSTNAME environment variable not found")
+	}
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		return err
+	}
+
+	client, err := azservicebus.NewClient(namespace, cred, nil)
 	if err != nil {
 		return err
 	}
@@ -45,23 +130,6 @@ func (Azure) ServiceBus() error {
 	return nil
 }
 
-type Test struct{}
-
-func (Test) GetServiceBusClientDefault() (*azservicebus.Client, error) {
-	namespace, ok := os.LookupEnv("AZURE_SERVICEBUS_HOSTNAME") //ex: myservicebus.servicebus.windows.net
-	if !ok {
-		return nil, errors.New("AZURE_SERVICEBUS_HOSTNAME environment variable not found")
-	}
-
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := azservicebus.NewClient(namespace, cred, nil)
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
+func (Azure) Postgres() error {
+	return errors.New("Not yet implemented")
 }
